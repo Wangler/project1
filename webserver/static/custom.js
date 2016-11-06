d24b025159136c91d769ed045157a6b101dc1485
Kaili Chen <kc3031@columbia.edu>
2016-11-06 18:21:54 -0500

    user login&logout&register&profile

--- webserver/static/custom.js
+++ webserver/static/custom.js
@@ -8,6 +8,10 @@ $(function () {
         if ($detailsTd.length) {
             $detailsTd.find("p").slideUp();
             $detailsTd.addClass("nopadding");
+            // $.getJSON($SCRIPT_ROOT, {
+            //     title: $('input[name="a"]').val(),
+            //   }, function(data) {
+            //     $("#result").text(data.result);
 
         } else {
             $detailsTd = $target.next().find("td[colspan=3]");
