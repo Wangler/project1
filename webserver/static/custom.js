d43e49c6fd5ca34738d0bdc268dd2ab972946f84
Lucy Wang <lucy.wang@buzzfeed.com>
2016-11-06 01:04:41 -0500

    updated templates and static

---
+++ webserver/static/custom.js
@@ -0,0 +1,17 @@
+$(function () {
+    $("td[class=3]").find("p").hide();
+    $("td[colspan=3]").addClass("nopadding");
+
+    $("tr").click(function () {
+        var $target = $(this);
+        var $detailsTd = $target.find("td[colspan=3]");
+        if ($detailsTd.length) {
+            $detailsTd.find("p").slideUp();
+            $detailsTd.addClass("nopadding");
+        } else {
+            $detailsTd = $target.next().find("td[colspan=3]");
+            $detailsTd.find("p").slideToggle();
+            $detailsTd.toggleClass("nopadding");
+        }
+    });
+});
